5d4300a341c1355a8f1cbee0f7b5f58d70c17944
Kevin Norris <kevin.norris.90@gmail.com>
2017-05-24 13:51:58 +1200

    Create api file with basic db debugging routes

---
+++ server/api.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const db = require('./db');
+
+const apiRoutes = express.Router();
+
+// Generic GET handler;
+function GET(url, handler) {
+  apiRoutes.get(url, (req, res) => {
+    handler(req)
+      .then((data) => {
+        res.json({
+          success: true,
+          data,
+        });
+      })
+      .catch((error) => {
+        res.json({
+          success: false,
+          error: error.message || error,
+        });
+      });
+  });
+}
+
+// API Routes
+
+
+// Debugging Routes
+
+// create table Users:
+GET('/users/create', db.users.create);
+
+// add some initial records:
+GET('/users/init', db.users.init);
+
+// remove all records from the table:
+GET('/users/empty', db.users.empty);
+
+// drop the table:
+GET('/users/drop', db.users.drop);
+
+// add a new user with username:
+GET('/users/add/:username', (req) => db.users.add([req.query.githubId, req.params.username, req.query.displayname]));
+
+// find a user with id:
+GET('/users/find/:id', (req) => db.users.find(req.params.id));
+
+module.exports = apiRoutes;
